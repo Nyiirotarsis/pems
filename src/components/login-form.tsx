90d321c24d559ae015000e5ee4fa182d1d7a0569
Nyiiro Tarsis <nyiirotarsis@gmail.com>
2025-09-09 14:29:12 +0000

    yes

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -41,7 +41,7 @@ export function LoginForm() {
   const form = useForm<z.infer<typeof loginFormSchema>>({
     resolver: zodResolver(loginFormSchema),
     defaultValues: {
-      username: "storemanager",
+      username: "ceo",
       password: "123",
     },
   });
