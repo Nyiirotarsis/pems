bb91f6d0f1eefe68af3a70c93e54cdb74d5fb84a
Nyiiro Tarsis <nyiirotarsis@gmail.com>
2025-09-13 17:17:55 +0000

    hr/admin  credentials

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -45,7 +45,7 @@ export function LoginForm() {
   const form = useForm<z.infer<typeof loginFormSchema>>({
     resolver: zodResolver(loginFormSchema),
     defaultValues: {
-      username: "it",
+      username: "hr",
       password: "123",
     },
   });
